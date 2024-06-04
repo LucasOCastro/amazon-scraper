05558fac4ab09557d4c0a5a07f32f4936c4ea3c3
lucas castro <lucas_o__castro@hotmail.com>
2024-06-04 11:09:10 -0300

    Uses cors in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
-import express from 'express'
-import http from 'http'
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
 import scrape from "./routes/scrape.js";
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/scrape', scrape);
