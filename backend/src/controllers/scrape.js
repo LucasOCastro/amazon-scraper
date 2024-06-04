fcea96d6bcebbb8f0ee05cbd8a0e773b928697fa
lucas castro <lucas_o__castro@hotmail.com>
2024-06-04 04:29:50 -0300

    Creates scrape endpoint

---
+++ backend/src/controllers/scrape.js
@@ -0,0 +1,77 @@
+import axios from "axios";
+import {JSDOM} from "jsdom";
+
+function extractStarCount(starElement){
+    //Iterate through each class until we find the class which mentions the star count.
+    //Then, use a regex to match and extract the numerical value of stars from the class name.
+    for (const cl of starElement.classList){
+        const match = cl.match(/^a-star-small-([0-4]-[0-5]|[0-5])$/);
+        if (match){
+            return match[1].replace('-', '.');
+        }
+    }
+    console.error('Error when extracting stars.');
+    return '-1';
+}
+
+function decomposeItem(item){
+    if (!item
+        .querySelector('.s-title-instructions-style')){
+        console.log(item.innerHTML);
+    }
+
+    //Find the title based on a unique class named s-title-instructions-style
+    // and then get the descendant with the actual text.
+    const title = item
+        .querySelector('.s-title-instructions-style')
+        .querySelector('span .a-text-normal')
+        .firstChild.innerHTML;
+
+    //Find the star element based on a unique class named a-icon-star-small
+    // and then get the extract the actual numerical count.
+    const starEl = item.querySelector('.a-icon-star-small');
+    const starCount = extractStarCount(starEl);
+
+    //Climb up the tree from the star element then get the sibling,
+    // as the review count is right after the star indicator.
+    // Then get the descendant with the actual text.
+    const reviewCount = starEl
+        .parentElement
+        .parentElement
+        .parentElement
+        .nextElementSibling
+        .querySelector('span .s-underline-text')
+        .firstChild.innerHTML;
+
+    //Find the image element based on a unique class named s-image and get its source.
+    const imageUrl = item.querySelector('.s-image').src;
+
+    return { title, starCount, reviewCount, imageUrl };
+}
+
+export async function getProducts(req, res){
+    //Extract and verify keyword from query.
+    const keyword = req.query.keyword;
+    if (!keyword){
+        return res.status(400).send('Missing keyword.');
+    }
+
+    try{
+        //Fetch with axios and create the dom with JSDOM.
+        const response = await axios.get(`https://www.amazon.com/s?k=${keyword}`);
+        const dom = new JSDOM(response.data);
+
+        //Scrape the actual html document by parsing all elements with the type attribute
+        // indicating a search result.
+        const document = dom.window.document;
+        const results = document
+            .querySelectorAll('[data-component-type="s-search-result"]');
+
+        //Parse and return the result.
+        const parsedResults = Array.from(results).map(decomposeItem);
+        return res.status(200).send(parsedResults);
+    } catch (err) {
+        console.error('Unhandled error when fetching amazon search.', err);
+        return res.status(500).send('Server error.');
+    }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import http from 'http'
+import scrape from "./routes/scrape.js";
 
 const app = express();
 app.use(express.json());
+
+app.use('/api/scrape', scrape);
 
 const server = http.createServer(app);
 const port = 8080;
